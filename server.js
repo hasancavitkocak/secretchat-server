ee54b5659961804ddd569c61fdba79914f59b63c
hasancavitkocak <122098839+hasancavitkocak@users.noreply.github.com>
2025-04-09 13:53:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ import { Server } from 'socket.io';
 import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { createClient } from '@supabase/supabase-js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
